176b52dc15e1c887183017e2101ae1fefd9b5e45
= <wtylermann@gmail.com>
2017-08-20 15:04:14 -0700

    Convert to a full python implementation that respects include_paths and exclude_paths

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM python:slim
+FROM python:alpine
+
+RUN pip install binaryornot==0.4.3
 
 COPY app /app
 
-RUN adduser -u 5000 app --disabled-password
+RUN adduser -u 5000 app -D
 RUN chown -R app:app /app
 
 USER app
--- app/run.py
+++ app/run.py
@@ -1,49 +1,101 @@
+import fnmatch
 import json
 import os
-import subprocess
 import sys
 
-WORDS = ['FIXME', 'TODO', 'HAX', 'HACK', 'XXX']
+from binaryornot.check import is_binary
 
 
-def parse_and_print_results(result_text):
-    for line in result_text.splitlines():
-        split_parts = line.split(':')
-        if len(split_parts) != 3:
-            # Unexpected output
-            continue
-        path, line_number, word = split_parts
-        print(json.dumps({
-            'name': 'comment with "{}"'.format(word),
-            'description': 'Your settings are set to point out comments with the term "{}"'.format(word),
-            'location': {
-                'path': os.path.normpath(path),
-                'start': {
-                    'line': line_number
-                },
-                'end': {
-                    'line': line_number
-                }
-            }
-        }))
+DEFAULT_WORDS = ['FIXME', 'TODO', 'HAX', 'HACK', 'XXX']
+
+
+def normalize_paths(paths):
+    return [p.lstrip('/') for p in paths]
+
+
+def parse_config():
+    with open('/engine_config.json', 'r') as fd:
+        engine_config = json.loads(fd.read())
+        include_paths = normalize_paths(engine_config.get('include_paths', []))
+        exclude_paths = normalize_paths(engine_config.get('exclude_paths', []))
+    return include_paths, exclude_paths
+
+
+def _should_include_path(path, include_paths, exclude_paths):
+    # First check if there are include paths
+    if include_paths:
+        for i_path in include_paths:
+            if fnmatch.fnmatch(path, i_path):
+                return True
+        return False
+
+    # Check exclude paths
+    for e_path in exclude_paths:
+        if fnmatch.fnmatch(path, e_path):
+            return False
+    return True
+
+
+def _report_line(file_path, line_number, word):
+    print(json.dumps({
+        'name': 'line containing "{}"'.format(word),
+        'description': 'Your settings are set to point out lines with the term "{}"'.format(word),
+        'location': {
+            'path': file_path,
+            'line': line_number
+        }
+    }))
+
+
+def search_file(patterns, file_path):
+    # Don't search binary files
+    if is_binary(file_path):
+        return
+
+    # Make sure the file is less than 1 MB
+    try:
+        if os.stat(file_path).st_size > 1024 * 1024:
+            return
+    except IOError:
+        # If we are for some reason unable to stat this file, then we probably won't be able to read it either
+        return
+
+    try:
+        with open(file_path, 'r') as fd:
+            matching_line = None
+            for line_number, line in enumerate(fd, 1):
+                if len(line) > 1000:
+                    # only allow lines less than 1000 characters, since greater than this is likely not source code
+                    continue
+
+                for pattern in patterns:
+                    if pattern in line:
+                        _report_line(file_path, line_number, line)
+                        break
+
+    except (ValueError, IOError):
+        pass
 
 
 def main():
-    try:
-        output = subprocess.check_output([
-            'grep',
-            '--binary-files=without-match',
-            '--only-matching',
-            '--with-filename',
-            '--extended-regexp',
-            '-rn',
-            '({})'.format('|'.join(WORDS)),
-            '.']
-        )
-        parse_and_print_results(output.decode().strip())
-    except subprocess.CalledProcessError as exc:
-        # No matches found
-        pass
+    patterns = []
+    for word in DEFAULT_WORDS:
+        patterns.append(word.upper())
+        #patterns.append(word.lower())
+        #patterns.append(word.title())
+
+    include_paths, exclude_paths = parse_config()
+
+    # Add the .git path to excludes since there are a bunch of text files
+    exclude_paths.append('.git/*')
+
+    for root, dirs, files in os.walk('.'):
+        root_dir = os.path.normpath(root)
+        for f in files:
+            path = os.path.join(root_dir, f)
+            if not _should_include_path(path, include_paths, exclude_paths):
+                continue
+            search_file(patterns, path)
 
 
 if __name__ == '__main__':
